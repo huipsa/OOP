f0c7809e207a99d0f4c60ce4b781cae2341547f5
huipsa <wetquartz@gmail.com>
2024-02-04 21:34:11 +0500

    added __str__ and others

--- oop.py
+++ oop.py
@@ -1,3 +1,6 @@
+from statistics import mean
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -19,6 +22,33 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return "Ошибка"
+
+    def avg_grade(self):
+        new_p = []
+        for k, v in self.grades.items():
+            new_p += v
+            new_z: float = sum(new_p) / len(new_p)
+            return new_z
+
+    def __str__(self):
+        courses_in_progress_string = ", ".join(self.courses_in_progress)
+        finished_courses_string = ", ".join(self.finished_courses)
+        return (
+            f"Имя:{self.name}\n"
+            f"Фамилия:{self.surname}\n"
+            f"Средняя оценка за домашнее задание:{self.avg_grade()}\n"
+            f"Курсы в процессе обучени:{courses_in_progress_string}\n"
+            f"Завершенные курсы:{finished_courses_string}"
+        )
+
+    def __lt__(self, other):
+        return self.avg_grade() < other.avg_grade()
+
+    def __le__(self, other):
+        return self.avg_grade() < other.avg_grade()
+
+    def __eq__(self, other):
+        return self.avg_grade() < other.avg_grade()
 
 
 class Mentor:
@@ -42,21 +72,35 @@ class Reviewer(Mentor):
         else:
             return "Ошибка"
 
+    def __str__(self):
+        return f"Имя:{self.name}\n" f"Фамилия:{self.surname}\n"
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
 
+    def avg_grade(self):
+        new_p = []
+        for k, v in self.grades.items():
+            new_p += v
+            new_z: float = sum(new_p) / len(new_p)
+            return new_z
 
-best_student = Student("Ruoy", "Eman", "your_gender")
-best_student.courses_in_progress += ["Python"]
+    def __str__(self):
+        return (
+            f"Имя:{self.name}\n"
+            f"Фамилия:{self.surname}\n"
+            f"Средняя оценка за домашнее задание:{self.avg_grade()}\n"
+        )
 
-cool_mentor = Mentor("Some", "Buddy")
-cool_mentor.courses_attached += ["Python"]
+    def __lt__(self, other):
+        return self.avg_grade() < other.avg_grade()
 
-cool_mentor.rate_hw(best_student, "Python", 10)
-cool_mentor.rate_hw(best_student, "Python", 10)
-cool_mentor.rate_hw(best_student, "Python", 10)
+    def __le__(self, other):
+        return self.avg_grade() < other.avg_grade()
 
-print(best_student.grades)
+    def __eq__(self, other):
+        return self.avg_grade() < other.avg_grade()
+
